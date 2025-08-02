09a493d8801258bd0ee07b8060a32de67c9d6ffd
Nikhil <ilnurkkh@gmail.com>
2025-08-02 14:31:12 +0000

    Initial Skeleton

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import chess
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/')
+board = chess.Board()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    while not board.is_game_over():
+    
+        if request.method == 'POST':
+            # Handle form submission here
+            move = chess.Move.from_uci(request.form.get('move'))
+            if move in board.legal_moves:
+                board.push(move)
+                return redirect('/')
+            else:
+                return redirect('/')
+            
+        return render_template('index.html')
+    
+    return render_template('result.html', result=board.result())
 
 if __name__ == '__main__':
     app.run(debug=True)
